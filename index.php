e4a8f549d53dc88dff3d8b46a34e637194850123
marmz <dario.marotta@gmail.com>
2022-03-24 17:38:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?
+
+	require_once '../../.lib_shared/php/dompdf/autoload.inc.php';
+	
+	
+	// reference the Dompdf namespace
+	use \Dompdf\Dompdf;
+
+
+	ob_start();
+	include "chart.html";
+	$html = ob_get_clean();
+
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
+	
+	$options = $dompdf->getOptions();
+	$options->setIsHtml5ParserEnabled(true);
+	$dompdf->setOptions($options);
+	
+	$dompdf->loadHtml($html);
+
+	// (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('A4', 'landscape');
+
+	// Render the HTML as PDF
+	$dompdf->render();
+
+	// Output the generated PDF to Browser
+	$dompdf->stream();
